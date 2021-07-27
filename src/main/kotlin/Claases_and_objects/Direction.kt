b2de3b48882233524c34734e46317f9b68a5319f
Yoon <organicferrum@gmail.com>
2021-07-27 17:40:40 +0900

    add enumerous class

---
+++ src/main/kotlin/Claases_and_objects/Direction.kt
@@ -0,0 +1,6 @@
+package Claases_and_objects
+
+enum class Direction(var value:Int) {
+    NORTH(1),SOUTH(2),EAST(3),WEST(4)
+}
+
---
+++ src/main/kotlin/Claases_and_objects/Index.kt
@@ -0,0 +1,8 @@
+package Claases_and_objects
+
+fun main(args: Array<String>){
+
+    var north_direction=1
+    if(north_direction==Direction.NORTH.value) println("Going North")
+    else println("No idea where you're going")
+}
